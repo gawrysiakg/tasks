9d8f487d489c29006be00f464662d67301a5442e
Grzegorz Gawrysiak <grzegorz.gawrysiak2@gmail.com>
2022-12-01 23:09:53 +0100

    22.2 Zadanie: ulozenie kodu

---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,14 @@
+package com.crud.tasks.domain;
+
+
+import lombok.Data;
+//
+//@Getter
+//@AllArgsConstructor
+//@NoArgsConstructor
+@Data
+public class TrelloBoardDto {
+
+    private String name;
+    private String id;
+}
