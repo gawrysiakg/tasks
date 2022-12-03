20c76180dbeef83f10c2a32c4ce0b6d3f0b05ac6
Grzegorz Gawrysiak <grzegorz.gawrysiak2@gmail.com>
2022-12-03 22:07:10 +0100

    22.4

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -3,14 +3,16 @@ package com.crud.tasks.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-//
-//@Getter
-//@AllArgsConstructor
-//@NoArgsConstructor
+
+
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloBoardDto {
